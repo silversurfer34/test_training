d139b06f9db9ad1d15e1c239026462d4b14bebbf
silversurfer34 <fabian.serin@gmail.com>
2021-03-10 21:53:37 +0100

    selenium test initial state

--- selenium_chrome/selenium_tests.py
+++ selenium_chrome/selenium_tests.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.fixture
 def driver():
-    _driver = driver = webdriver.Chrome()
+    _driver = webdriver.Chrome()
     yield _driver
     _driver.close()
 
@@ -17,12 +17,3 @@ def test_search_in_python_org(driver):
     elem.send_keys("pytest")
     elem.send_keys(Keys.RETURN)
     assert "No results found." not in driver.page_source
-
-
-def test_fail_search_in_python_org(driver):
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
-    elem.send_keys("totototo")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." in driver.page_source
