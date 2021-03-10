1032b5477cec85cec6d0e7807fc44a6842119f51
silversurfer34 <fabian.serin@gmail.com>
2021-03-10 21:49:02 +0100

    locust test initial state

--- locust_files/my_locust_file.py
+++ locust_files/my_locust_file.py
@@ -1,26 +1,12 @@
 from locust import HttpUser, task, between
-from random import randint
 
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
-    @task(2)
+    @task()
     def get_available_pets(self):
         self.client.get("/v2/pet/findByStatus?status=available")
-
-    @task(3)
-    def get_pending_pets(self):
-        self.client.get("/v2/pet/findByStatus?status=pending")
-
-    @task
-    def get_sold_pets(self):
-        self.client.get("/v2/pet/findByStatus?status=sold")
-
-    @task
-    def get_pet_by_id(self):
-        pet_id = randint(1, 20)
-        self.client.get(f"/v2/pet/{pet_id}")
 
     def on_start(self):
         pass
