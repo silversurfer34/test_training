5c8722f728c2b9061334221f12df84deb60d081f
silversurfer34 <fabian.serin@gmail.com>
2021-03-17 20:32:20 +0100

    add sorting use cases

--- perfo/perfo.py
+++ perfo/perfo.py
@@ -20,7 +20,7 @@ def time_sorting_algorithm(algorithm, array):
     #   number=10 will measure the time spend for 10 launches of stmt
     # repeat is the number of repetition of the measure
     #   repeat=10 will measure 10 times and return 10 results
-    times = repeat(setup=setup_code, stmt=stmt, repeat=5, number=1)
+    times = repeat(setup=setup_code, stmt=stmt, repeat=1, number=1)
     # Finally, display the name of the algorithm and the
     # minimum, max, average time it took to run
     print(
@@ -29,17 +29,27 @@ def time_sorting_algorithm(algorithm, array):
     )
 
 
-ARRAY_LENGTH = 100
+ARRAY_LENGTH = 10000
 
 
 if __name__ == "__main__":
     # Generate an array of `ARRAY_LENGTH` items consisting
     # of random integer values between 0 and 999
-    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
-    # Call the function using the name of the sorting algorithm
-    # and the array you just created
-    time_sorting_algorithm(algorithm="sorted", array=array)
-    time_sorting_algorithm(algorithm="bubble_sort", array=array)
-    time_sorting_algorithm(algorithm="insertion_sort", array=array)
-    time_sorting_algorithm(algorithm="merge_sort", array=array)
-    time_sorting_algorithm(algorithm="quicksort", array=array)
+    random_array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+    sorted_array = [i for i in range(ARRAY_LENGTH)] #Best for bubble
+    reverse_array = sorted_array[::-1] #Worst for insertion
+    min_at_the_end = sorted_array[::] #Worst for bubble
+    min_at_the_end.append(0)
+
+    use_cases = {"sorted array": sorted_array, "random_array": random_array, "reverse array": reverse_array,
+                 "min at the end": min_at_the_end}
+    for label, array in use_cases.items():
+        # Call the function using the name of the sorting algorithm
+        # and the array you just created
+        print(label)
+        # time_sorting_algorithm(algorithm="sorted", array=array)
+        time_sorting_algorithm(algorithm="bubble_sort", array=array)
+        time_sorting_algorithm(algorithm="insertion_sort", array=array)
+        time_sorting_algorithm(algorithm="merge_sort", array=array)
+        time_sorting_algorithm(algorithm="quicksort", array=array)
+        print("\n")
